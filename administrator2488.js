330f15be3faac114825b11df485587e6630b0a55
OpenShift guest <pymeswd@gmail.com>
2012-12-17 01:16:57 -0500

    17/12/12 1:16am

---
+++ administrator2488.js
@@ -0,0 +1,4 @@
+
+if(text == 2583 )
+{ window.location.href = llegadaformulario.html ;
+}
---
+++ llegadaformulario.js
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8" />
+<head>
+  <meta charset="utf-8">
+  <title> BrainShare</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Le styles -->
+  <link href="css_styles/simplex.css" rel="stylesheet">
+  <link href="css_styles/custom.css" rel="stylesheet">
+  <link href="brainshare.css" rel="stylesheet"> 
+  <style type="text/css">
+  body {
+    padding-top: 60px;
+    padding-bottom: 40px;
+  }
+  </style>
+  <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+  
+  <script type="text/javascript" src="function/jquery-1.8.3.min.js"></script>
+  <script type="text/javascript" src="function/brainshare.js"></script>
+  
+  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <![endif]-->
+
+      <!-- Fav and touch icons -->
+      <link rel="icon" type="image/ico" href="favicon.ico">
+      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+      
+    </head> 
+
+    <body>
+      
+      <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="navbar-inner">
+          <div class="container">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </a>
+            <a class="brand" href="#">BrainShare
+              
+            </a>
+            
+            <div class="container" style="width: auto;">
+              <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </a>
+              <a class="brand" href="#"></a>
+              <div class="nav-collapse">
+                <ul class="nav">
+                  <li class="active"><a href="brain_share.html">Home</a></li>
+                  <li><a href="#">Students</a></li>
+                  <li><a href="#">Teachers</a></li>
+                  <li><a href="#">topics</a></li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">Action</a></li>
+                      <li><a href="#">Another action</a></li>
+                      <li><a href="#">Something else here</a></li>
+                      <li class="divider"></li>
+                      <li><a href="#">Separated link</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                <form class="navbar-search pull-left" action="">
+                  <input type="text" class="search-query span2" placeholder="Search a contact">
+                </form>
+                <ul class="nav pull-right">
+                  <li><a href="#">Messages</a></li>
+                  <li class="divider-vertical"></li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Options <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">Profile Settings</a></li>
+                      <li><a href="#">Security Settings</a></li>
+                      <li><a href="#">Change Profile </a></li>
+                      <li class="divider"></li>
+                      <li><a href="index.html">Sign out</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div><!-- /.nav-collapse -->
+            </div>
+          </form>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+  </div>
+  <div class="well sidebar-nav">
+    <ul class="nav nav-list">
+      <input type="text" class="search-query span2" placeholder="Search (start with capital)" id="busqueda">
+      <button class="btn" id="buscar">Buscar</button>
+      
+      
+      
+      <h2>Topics</h2>
+      
+      <li class="nav-header">science</li>
+      <li class="active"><a href="pagina.html">cells breathing</a></li>
+      <li><a href="pagina.html">Animals</a></li>
+      <li><a href="pagina.html">Cells</a></li>
+      <li><a href="pagina.html">Heart</a></li>
+      <li class="nav-header">Math</li>
+      <li><a href="pagina.html">Baldor Algebra</a></li>
+      <li><a href="pagina.html">Algorithms</a></li>
+      <li><a href="pagina.html">factorization</a></li>
+      <li><a href="pagina.html">Límite de una función </a></li>
+      <li><a href="pagina.html">Honorificus calculus </a></li>
+      <li><a href="pagina.html">Systems of linear equations</a></li>
+      <li class="nav-header">physics</li>
+      <li><a href="pagina.html">String theory</a></li>
+      <li><a href="pagina.html">Fall</a></li>
+      <li><a href="pagina.html">Units Transformation</a></li>
+    </div>
+    
+    
+    
+    
+    
+    
+    <div class="container">
+      <div class="row">
+        <div class="span3"> 
+          <img class="profilePic img" src="http://4.bp.blogspot.com/-B2czanc3VGU/TV57_1LbPkI/AAAAAAAAASI/wkL10nuJP3Q/s1600/4644.jpg" heigth="350px" width="250px"   alt="David Velasquez" id="propho" >
+          <a class="btn btn-info btn-large" href="mailto:davidvelasquezm.6@gmail.com">Send a Message</a>    
+          <a class="btn btn-warning btn-large" href="#">hire(Free)</a>
+          
+        </div>
+        
+        <ul class="nav nav-list">
+          <div class="name span6">
+            
+
+
+            <li><h1>Name</h1> </li>
+            <li id="nombr"><h3><strong>David Velasquez</strong></h3></li>
+            <li><h1>Experience Area:</h1></li>
+            <li id="expa"><h3><strong>Web development</strong></h3></li>
+            <li><h1>Working in:</h1></li>
+            <li id="wori"><h3>BrainShare (creator )</h3></li>
+            <h1>professional experience:</h1></li>
+            <ul>
+              <li><h3><strong>Facebook (web Developer)</strong></h3>
+                <li><h3><strong> independent (Software Developer)</strong></h3>
+                </ul>
+                <h1>School:</h1></li>
+                <li><h3><strong>Colegio Aleman</strong></h3></li>
+                <h1>studies:</h1></li>
+                <ul>
+                  <li><h3><strong>Universidad de antioquia (sistems engenner)</strong></h3>
+                   <li><h3><strong></strong></h3>
+                   </ul>
+                 </div>
+                 
+                 
+                 
+                 
+                 
+                 
+                 <!-- Main hero unit for a primary marketing message or call to action -->
+                 
+                 
+                 
+                 
+                 
+
+                 
+                 
+                 
+
+                 
+
+ <!-- Le javascript
+ ================================================== -->
+ <!-- Placed at the end of the document so the pages load faster -->
+ <script type="text/javascript" src="function/jquery-1.8.3.min.js"></script>
+ <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+ <!-- Load Stackmob Dependencies -->
+ <script type="text/javascript" src="http://static.stackmob.com/js/json2-min.js"></script>
+ <script type="text/javascript" src="http://static.stackmob.com/js/underscore-1.4.1-min.js"></script>
+ <script type="text/javascript" src="http://static.stackmob.com/js/backbone-0.9.2-min.js"></script>
+ <script type="text/javascript" src="http://static.stackmob.com/js/stackmob-js-0.6.0-min.js"></script>
+ <!-- App scripts (Disable until they finnish their design) -->
+ <script type="text/javascript" src="function/brainshare.js"></script>
+</body>
+</html>
